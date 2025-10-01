14bdf1d471e8f31c449aa210e7f246036a5c08f6
reismaa <apriliandv07@gmail.com>
2025-10-01 10:04:48 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS24.java
+++ jobsheet5/ifCetakKRS24.java
@@ -9,13 +9,8 @@ public class ifCetakKRS24 {
     System.out.println("Apakah UKT sudah lunas? (true/false): ");
     boolean uktLunas = sc.nextBoolean();
 
-    if (uktLunas) {
-        System.out.println("Pembayaran UKT terverifikasi");
-        System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-    }
-    else {
-        System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-    }
+    System.out.println(uktLunas ? "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu") ;
+    
     sc.close();
     }
     

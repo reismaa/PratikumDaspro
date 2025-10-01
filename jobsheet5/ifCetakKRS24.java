c3b2309ec28a0af639a552dedde0bf1ad502bb90
reismaa <apriliandv07@gmail.com>
2025-10-01 08:10:34 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS24.java
+++ jobsheet5/ifCetakKRS24.java
@@ -13,6 +13,9 @@ public class ifCetakKRS24 {
         System.out.println("Pembayaran UKT terverifikasi");
         System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
     }
+    else {
+        System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+    }
     sc.close();
     }
     

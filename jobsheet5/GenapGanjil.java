475f2af17622e054805e3141e5f11a21cc338ed1
reismaa <apriliandv07@gmail.com>
2025-10-01 10:28:22 +0700

    Tugas 2

---
+++ jobsheet5/GenapGanjil.java
@@ -0,0 +1,20 @@
+package jobsheet5;
+import java.util.Scanner;
+
+public class GenapGanjil {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
+
+        sc.close();
+    }
+    
+}

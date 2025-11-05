3127c69569026ea8bea6d48f346c49ccae771792
reismaa <apriliandv07@gmail.com>
2025-11-05 12:32:45 +0700

    add modifikasi

--- jobsheet9/ArrayRataNillai24.java
+++ jobsheet9/ArrayRataNillai24.java
@@ -9,6 +9,7 @@ public class ArrayRataNillai24 {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        int jumlahLulus = 0; // menghitung mahasiswa lulus
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
@@ -17,10 +18,14 @@ public class ArrayRataNillai24 {
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                jumlahLulus++;
+            }
         }
 
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata-rata nilai = "+rata2);
+        System.out.println("Jumlah mahasiswa yang lulus = " + jumlahLulus);
 
         sc.close();
     }

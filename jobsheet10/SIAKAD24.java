68314219931b9ab080b3e331dd6d47cca4b5a35b
reismaa <apriliandv07@gmail.com>
2025-11-12 16:42:17 +0700

    modifikasi

--- jobsheet10/SIAKAD24.java
+++ jobsheet10/SIAKAD24.java
@@ -6,35 +6,45 @@ public class SIAKAD24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[][] nilai = new int [4][3];
+        // Meminta jumlah siswa dan mata kuliah
+        System.out.print("Masukkan jumlah siswa: ");
+        int jumlahSiswa = sc.nextInt();
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMatkul = sc.nextInt();
 
-        for (int i = 0; i < nilai.length; i++) {
-            System.out.println("Input nilai mahasiswa ke-" + (i + 1));
+        // Deklarasi array dengan ukuran dinamis
+        int[][] nilai = new int[jumlahSiswa][jumlahMatkul];
+
+        // Input nilai dan hitung rata-rata per siswa
+        for (int i = 0; i < jumlahSiswa; i++) {
+            System.out.println("\nInput nilai mahasiswa ke-" + (i + 1));
             double totalPerSiswa = 0;
 
-            for (int j = 0; j < nilai[i].length; j++) {
-                System.out.print("Nilai mata kuliah " + (j+1) + ": ");
+            for (int j = 0; j < jumlahMatkul; j++) {
+                System.out.print("Nilai mata kuliah " + (j + 1) + ": ");
                 nilai[i][j] = sc.nextInt();
                 totalPerSiswa += nilai[i][j];
             }
 
-            System.out.println("Nilai rata-rata: " + totalPerSiswa/3);
+            double rataPerSiswa = totalPerSiswa / jumlahMatkul;
+            System.out.println("Rata-rata mahasiswa ke-" + (i + 1) + ": " + rataPerSiswa);
         }
 
+        // Menghitung rata-rata setiap mata kuliah
         System.out.println("\n====================================");
         System.out.println("Rata-rata Nilai setiap Mata Kuliah:");
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < jumlahMatkul; j++) {
             double totalPerMatkul = 0;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < jumlahSiswa; i++) {
                 totalPerMatkul += nilai[i][j];
             }
 
-            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul / 4);
+            double rataPerMatkul = totalPerMatkul / jumlahSiswa;
+            System.out.println("Mata Kuliah " + (j + 1) + ": " + rataPerMatkul);
         }
 
         sc.close();
     }
-    
 }

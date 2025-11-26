9210d030c1698e7790c7ee68427a1b81af8d5468
reismaa <apriliandv07@gmail.com>
2025-11-26 10:13:21 +0700

    modifikasi pertanyaan nomor 3 percobaan 3

--- Pertemuan14/Kafe24.java
+++ Pertemuan14/Kafe24.java
@@ -31,13 +31,33 @@ public class Kafe24 {
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
     }
 
-    // modifikasi nomor 2
-    public static int hitungTotalHarga24(int pilihanMenu, int banyakItem) {
-        int [] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+    // modifikasi pertanyaan nomor 3 percobaan 3
+    public static int hitungTotalHarga24(int pilihanMenu, int banyakItem, String kodePromo) {
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+
+        // Cek menu valid
+        if (pilihanMenu < 1 || pilihanMenu > hargaItems.length) {
+             System.out.println("Nomor menu tidak valid!");
+                return 0;
+        }
 
         int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
-        return hargaTotal;
-    }
+        int diskon = 0;
+
+        // Cek kode promo
+        if (kodePromo.equalsIgnoreCase("DISKON50")) {
+            diskon = hargaTotal / 2; // 50%
+            System.out.println("Diskon 50% diterapkan: -Rp" + diskon);
+        } else if (kodePromo.equalsIgnoreCase("DISKON30")) {
+            diskon = (int)(hargaTotal * 0.3); // 30%
+            System.out.println("Diskon 30% diterapkan: -Rp" + diskon);
+        } else {
+            System.out.println("Kode promo invalid. Tidak ada diskon.");
+        }
+
+        return hargaTotal - diskon;
+}
+
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -50,7 +70,7 @@ public class Kafe24 {
         System.out.println("Masukkan jumlah item yang ingin dipesan: ");
         int banyakItem = sc.nextInt();
 
-        int totalharga = hitungTotalHarga24(pilihanMenu, banyakItem);
+        int totalharga = hitungTotalHarga24(pilihanMenu, banyakItem, null);
 
         System.out.println("Total harga untuk pesanan Anda: Rp" + totalharga);
 

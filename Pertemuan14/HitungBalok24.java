baeb279de05606183edfdb23ea8e1bebe885da0d
reismaa <apriliandv07@gmail.com>
2025-11-26 11:23:56 +0700

    3 fungsi

--- Pertemuan14/HitungBalok24.java
+++ Pertemuan14/HitungBalok24.java
@@ -3,8 +3,23 @@ package Pertemuan14;
 import java.util.Scanner;
 
 public class HitungBalok24 {
+
+    // fungsi hitungluas
+    static int hitungluas(int pjg, int lb) {
+        int Luas = pjg * lb;
+        return Luas;
+    }
+
+    // fungsi hitungVolume
+    static int hitungVolume(int tinggi, int a, int b) {
+        int volume = hitungluas(a, b) * tinggi;
+        return volume;
+    }
+
+    // fungsi main
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         int p, l, t, L, vol;
 
         System.out.println("Masukkan panjang");
@@ -16,9 +31,10 @@ public class HitungBalok24 {
         System.out.println("Masukkan tinggi");
         t = input.nextInt();
 
-        L = p * l;
+        L = hitungluas(p, l);
         System.out.println("Luas persegi panjang adalah " + L);
-        vol = p * l * t;
+
+        vol = hitungVolume(t, p, l);
         System.out.println("Volume balok adalah " + vol);
 
         input.close();

b9b6219f1c5f361b6339e96c5af48164d7a17598
reismaa <apriliandv07@gmail.com>
2025-11-26 10:52:56 +0700

    add new files

---
+++ Pertemuan14/PengunjungCafe24.java
@@ -0,0 +1,18 @@
+package Pertemuan14;
+
+public class PengunjungCafe24 {
+
+    static void daftarPengunjung(String...namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung:");
+        for(int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]) ;
+        }
+    }
+
+    public static void main(String[] args) {
+        daftarPengunjung("Ali","Budi","Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+    
+}
